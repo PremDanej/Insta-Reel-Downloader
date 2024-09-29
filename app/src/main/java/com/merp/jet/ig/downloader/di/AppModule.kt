535643eb4e2b6a5c39fc063a031eab41283a7679
PremDanej <danejprem005@gmail.com>
2024-09-30 00:47:18 +0530

    AppModule added.

---
+++ app/src/main/java/com/merp/jet/ig/downloader/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.merp.jet.ig.downloader.di
+
+import com.merp.jet.ig.downloader.network.ReelApi
+import com.merp.jet.ig.downloader.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideReelApi(): ReelApi {
+
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ReelApi::class.java)
+    }
+}
