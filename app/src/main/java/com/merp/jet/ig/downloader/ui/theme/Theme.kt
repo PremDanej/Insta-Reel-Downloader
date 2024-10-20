6cc7959adc4104cfe16c6ab778014227893a2bc0
PremDanej <danejprem005@gmail.com>
2024-10-20 16:24:43 +0530

    Primary color added for DayNight mode theme.

--- app/src/main/java/com/merp/jet/ig/downloader/ui/theme/Theme.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.merp.jet.ig.downloader.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = LightBackground,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = DarkBackground,
+    onBackground = LightBackground,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = DarkBackground,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = LightBackground,
+    onBackground = DarkBackground,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
