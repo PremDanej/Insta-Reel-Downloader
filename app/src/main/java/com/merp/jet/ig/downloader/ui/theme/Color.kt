132e983e58889e29595bda41f86a8cadb8e50982
PremDanej <danejprem005@gmail.com>
2024-10-20 16:23:03 +0530

    New color added.

--- app/src/main/java/com/merp/jet/ig/downloader/ui/theme/Color.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.merp.jet.ig.downloader.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+// Dark
+val PurpleGrey80 = Color(0xFF3C3532)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+// Light
+val PurpleGrey40 = Color(0xFF5A574F)
 val Pink40 = Color(0xFF7D5260)
+
+val LightBackground = Color(0xFFFBF3DC)
+val DarkBackground = Color(0xFF110600)
