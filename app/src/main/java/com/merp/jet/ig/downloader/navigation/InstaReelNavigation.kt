2e9a957a2574f3e47d51fffe5fc11a2028811e52
PremDanej <danejprem005@gmail.com>
2024-10-22 19:09:15 +0530

    Setting up navigation.

---
+++ app/src/main/java/com/merp/jet/ig/downloader/navigation/InstaReelNavigation.kt
@@ -0,0 +1,32 @@
+package com.merp.jet.ig.downloader.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.merp.jet.ig.downloader.screens.about.AboutScreen
+import com.merp.jet.ig.downloader.screens.reel.ReelScreen
+import com.merp.jet.ig.downloader.screens.save.SaveScreen
+import com.merp.jet.ig.downloader.screens.setting.SettingScreen
+
+@Composable
+fun InstaReelNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = InstaReelScreens.ReelScreen.name) {
+
+        composable(route = InstaReelScreens.ReelScreen.name) {
+            ReelScreen(navController)
+        }
+        composable(route = InstaReelScreens.AboutScreen.name) {
+            AboutScreen(navController)
+        }
+        composable(route = InstaReelScreens.SaveScreen.name) {
+            SaveScreen(navController)
+        }
+        composable(route = InstaReelScreens.SettingScreen.name) {
+            SettingScreen(navController)
+        }
+    }
+}
