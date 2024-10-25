a0ef92d931d8c5f4f28381a7ef2938f4587ce29d
PremDanej <danejprem005@gmail.com>
2024-10-25 21:27:54 +0530

    Setting up ReelScreen.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/reel/ReelScreen.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/reel/ReelScreen.kt
@@ -1,10 +1,37 @@
 package com.merp.jet.ig.downloader.screens.reel
 
+import android.content.Context
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.merp.jet.ig.downloader.R
+import com.merp.jet.ig.downloader.components.LoadingButton
 import com.merp.jet.ig.downloader.components.ScreenDefault
 
 @Composable
@@ -14,6 +41,52 @@ fun ReelScreen(navController: NavController, viewModel: ReelViewModel = hiltView
         isMainScreen = true,
         navController = navController
     ) {
-        // Content
+        ScreenContent(viewModel = viewModel)
     }
+}
+
+@Composable
+fun ScreenContent(viewModel: ReelViewModel) {
+    val context = LocalContext.current
+    val keyboard = LocalSoftwareKeyboardController.current
+    var videoLink by remember { mutableStateOf("") }
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        OutlinedTextField(
+            value = videoLink,
+            onValueChange = { videoLink = it },
+            label = { Text(text = "Reel Link") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+            keyboardActions = KeyboardActions {
+                keyboard?.hide()
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LoadingButton(
+            text = stringResource(R.string.lbl_get),
+            enabled = !viewModel.isLoading,
+            isLoading = viewModel.isLoading
+        ) {
+            if (videoLink.isEmpty() || !URLUtil.isValidUrl(videoLink)) {
+                showToast(context, message = "Please enter a valid link")
+            }
+        }
+    }
+}
+
+
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
