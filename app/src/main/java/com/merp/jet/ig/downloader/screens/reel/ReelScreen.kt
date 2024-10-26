bb1c6b7febc13b405113c256b2a85078b7480555
PremDanej <danejprem005@gmail.com>
2024-10-26 22:47:54 +0530

    Downloading functionality added.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/reel/ReelScreen.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/reel/ReelScreen.kt
@@ -1,8 +1,13 @@
 package com.merp.jet.ig.downloader.screens.reel
 
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Base64
+import android.util.Log
 import android.webkit.URLUtil
 import android.widget.Toast
 import androidx.compose.foundation.Image
@@ -46,7 +51,15 @@ import com.merp.jet.ig.downloader.R
 import com.merp.jet.ig.downloader.components.LoadingButton
 import com.merp.jet.ig.downloader.components.ScreenDefault
 import com.merp.jet.ig.downloader.model.ReelResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 @Composable
 fun ReelScreen(navController: NavController, viewModel: ReelViewModel = hiltViewModel()) {
@@ -172,11 +185,64 @@ fun ReelDownloading(reelResponse: ReelResponse) {
                 isLoading = isDownloading
             ) {
                 isDownloading = true
+                downloadReel(media.url, context) { isDownloading = false }
             }
         }
     }
 }
 
+fun downloadReel(videoLink: String, context: Context, onComplete: () -> Unit) {
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val url = URL(videoLink)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.connectTimeout = 5000
+
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val contentValue = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME,"InstaReel_${System.currentTimeMillis()}.mp4")
+                    put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, "Movies/InstaReels")
+                }
+
+                val resolver: ContentResolver = context.contentResolver
+                val uri: Uri? =
+                    resolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, contentValue)
+
+                uri?.let { validUri ->
+                    resolver.openOutputStream(validUri)?.use { outputStream ->
+                        connection.inputStream.use { input ->
+                            input.copyTo(outputStream)
+                        }
+                    }
+                    withContext(Dispatchers.Main) {
+                        showToast(context, message = "Reel Downloaded")
+                    }
+                } ?: run {
+                    withContext(Dispatchers.Main) {
+                        showToast(context, message = "Failed to download reel")
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    showToast(context, message = "Failed to download reel")
+                }
+            }
+        } catch (e: MalformedURLException) {
+            Log.e("REEL", "Malformed URL: ${e.message}")
+            withContext(Dispatchers.Main) {
+                showToast(context, message = "Invalid URL")
+            }
+        } catch (e: IOException) {
+            Log.e("REEL", "IO Exception: ${e.message}")
+            withContext(Dispatchers.Main) {
+                showToast(context, message = "Something went wrong")
+            }
+        } finally {
+            onComplete()
+        }
+    }
+}
 
 fun showToast(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
