d8bf1195ea754ea4ef68086952b229119ae153c2
PremDanej <danejprem005@gmail.com>
2024-10-27 15:22:21 +0530

    SettingScreen added.

---
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
@@ -0,0 +1,42 @@
+package com.merp.jet.ig.downloader.screens.setting
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import com.merp.jet.ig.downloader.R
+import com.merp.jet.ig.downloader.components.ScreenDefault
+
+@Composable
+fun SettingScreen(
+    navController: NavController,
+    isDark: MutableState<Boolean>
+) {
+    ScreenDefault(
+        title = stringResource(id = R.string.lbl_setting),
+        isMainScreen = false,
+        icon = Icons.AutoMirrored.Default.ArrowBack,
+        navController = navController,
+        onBackPressed = { navController.popBackStack() }
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) { 
+            Text(text = "Setting Screen")
+        }
+    }
+}
