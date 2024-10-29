ce1fa7fe852e3c950ddfac2c9f0a8ba1893dff3b
PremDanej <danejprem005@gmail.com>
2024-10-29 23:42:35 +0530

    SaveScreen added.

---
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/save/SaveScreen.kt
@@ -0,0 +1,9 @@
+package com.merp.jet.ig.downloader.screens.save
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SaveScreen(navController: NavController) {
+
+}
