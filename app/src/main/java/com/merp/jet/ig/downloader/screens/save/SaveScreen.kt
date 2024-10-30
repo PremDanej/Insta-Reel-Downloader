82ea4252c8fc1caf66741505356657525c56a5f8
PremDanej <danejprem005@gmail.com>
2024-10-30 21:08:46 +0530

    Setting up SaveScreen.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/save/SaveScreen.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/save/SaveScreen.kt
@@ -1,9 +1,38 @@
 package com.merp.jet.ig.downloader.screens.save
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.merp.jet.ig.downloader.R
+import com.merp.jet.ig.downloader.components.ScreenDefault
 
 @Composable
 fun SaveScreen(navController: NavController) {
-
+    ScreenDefault(
+        title = stringResource(id = R.string.lbl_save),
+        isMainScreen = false,
+        icon = Icons.AutoMirrored.Default.ArrowBack,
+        navController = navController,
+        onBackPressed = { navController.popBackStack() }
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Save Screen")
+        }
+    }
 }
