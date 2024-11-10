23d5b2fd1b52e676b0c53725982b84895e4385c7
PremDanej <danejprem005@gmail.com>
2024-11-10 17:13:47 +0530

    SettingScreen updated.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
@@ -7,12 +7,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.LightMode
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -23,7 +28,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.merp.jet.ig.downloader.R
+import com.merp.jet.ig.downloader.R.string.lbl_dark_mode
+import com.merp.jet.ig.downloader.R.string.lbl_light_mode
+import com.merp.jet.ig.downloader.R.string.lbl_setting
+import com.merp.jet.ig.downloader.components.BACKGROUND_COLOR
+import com.merp.jet.ig.downloader.components.ON_BACKGROUND_COLOR
 import com.merp.jet.ig.downloader.components.ScreenDefault
 
 @Composable
@@ -32,7 +41,7 @@ fun SettingScreen(
     isDark: MutableState<Boolean>
 ) {
     ScreenDefault(
-        title = stringResource(id = R.string.lbl_setting),
+        title = stringResource(lbl_setting),
         isMainScreen = false,
         icon = Icons.AutoMirrored.Default.ArrowBack,
         navController = navController,
@@ -55,8 +64,8 @@ fun SettingScreen(
 fun ThemeOption(modifier: Modifier = Modifier) {
     val themeOption = listOf(
         "System default",
-        stringResource(R.string.lbl_light_mode),
-        stringResource(R.string.lbl_dark_mode)
+        stringResource(lbl_light_mode),
+        stringResource(lbl_dark_mode)
     )
     val (selectOption, onOptionSelected) = remember {
         mutableStateOf(themeOption[0])
@@ -84,17 +93,36 @@ fun ChangeTheme(checked: MutableState<Boolean>, onClick: (Boolean) -> Unit = {})
         Arrangement.SpaceBetween,
         Alignment.CenterVertically
     ) {
-        val theme =
-            if (checked.value)
-                stringResource(id = R.string.lbl_light_mode)
-            else
-                stringResource(id = R.string.lbl_dark_mode)
-        Text(text = "Turn to $theme mode")
+        Text(text = if(checked.value) "Turn off dark mode" else "Turn on dark mode")
         Switch(
             checked = checked.value,
             onCheckedChange = {
                 onClick(it)
-            }
+            },
+            thumbContent = {
+                if (checked.value) {
+                    Icon(
+                        imageVector = Icons.Filled.DarkMode,
+                        contentDescription = null,
+                        modifier = Modifier.size(SwitchDefaults.IconSize),
+                    )
+                }else{
+                    Icon(
+                        imageVector = Icons.Filled.LightMode,
+                        contentDescription = null,
+                        modifier = Modifier.size(SwitchDefaults.IconSize),
+                    )
+                }
+            },
+            colors = SwitchDefaults.colors(
+                checkedBorderColor = ON_BACKGROUND_COLOR,
+                uncheckedBorderColor = ON_BACKGROUND_COLOR,
+                checkedIconColor = ON_BACKGROUND_COLOR,
+                checkedTrackColor = ON_BACKGROUND_COLOR,
+                uncheckedTrackColor = BACKGROUND_COLOR,
+                checkedThumbColor = BACKGROUND_COLOR,
+                uncheckedThumbColor = ON_BACKGROUND_COLOR,
+            )
         )
     }
 }
