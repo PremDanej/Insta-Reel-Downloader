f54bbb665aa2b6bb1c0ab5bd59ad2959415eaf0e
PremDanej <danejprem005@gmail.com>
2024-12-08 16:01:06 +0530

    Font style added.

--- app/src/main/java/com/merp/jet/ig/downloader/ui/theme/Type.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/ui/theme/Type.kt
@@ -2,33 +2,56 @@ package com.merp.jet.ig.downloader.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.merp.jet.ig.downloader.R
 
-// Set of Material typography styles to start with
+val baseFont = FontFamily(Font(R.font.poppins))
+
 val Typography = Typography(
+
+    titleLarge = TextStyle(
+        fontFamily = baseFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = baseFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 10.sp,
+        letterSpacing = 0.sp
+    ),
+
+    // InputText value style
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = baseFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+
+    // InputText label style
+    bodySmall = TextStyle(
+        fontFamily = baseFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.sp
+    ),
+
+    // Button text style
+    labelLarge = TextStyle(
+        fontFamily = baseFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
 )
