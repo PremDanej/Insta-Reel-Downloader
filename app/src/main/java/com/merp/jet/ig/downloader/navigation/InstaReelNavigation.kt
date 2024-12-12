b6b99470442428ae2aa82290254d5f9de9e14ca2
PremDanej <danejprem005@gmail.com>
2024-12-12 17:22:31 +0530

    DynamicColor changes functionality added.

--- app/src/main/java/com/merp/jet/ig/downloader/navigation/InstaReelNavigation.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/navigation/InstaReelNavigation.kt
@@ -2,32 +2,36 @@ package com.merp.jet.ig.downloader.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.merp.jet.ig.downloader.navigation.InstaReelScreens.AboutScreen
+import com.merp.jet.ig.downloader.navigation.InstaReelScreens.ReelScreen
+import com.merp.jet.ig.downloader.navigation.InstaReelScreens.SaveScreen
+import com.merp.jet.ig.downloader.navigation.InstaReelScreens.SettingScreen
 import com.merp.jet.ig.downloader.screens.about.AboutScreen
 import com.merp.jet.ig.downloader.screens.reel.ReelScreen
 import com.merp.jet.ig.downloader.screens.save.SaveScreen
 import com.merp.jet.ig.downloader.screens.setting.SettingScreen
 
 @Composable
-fun InstaReelNavigation(isDark: MutableState<Boolean>) {
-
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = InstaReelScreens.ReelScreen.name) {
-
-        composable(route = InstaReelScreens.ReelScreen.name) {
+fun InstaReelNavigation(
+    isDark: MutableState<Boolean>,
+    isDynamicColor: MutableState<Boolean>,
+    navController: NavHostController
+) {
+    NavHost(navController, ReelScreen.name) {
+        composable(route = ReelScreen.name) {
             ReelScreen(navController)
         }
-        composable(route = InstaReelScreens.AboutScreen.name) {
+        composable(route = AboutScreen.name) {
             AboutScreen(navController)
         }
-        composable(route = InstaReelScreens.SaveScreen.name) {
+        composable(route = SaveScreen.name) {
             SaveScreen(navController)
         }
-        composable(route = InstaReelScreens.SettingScreen.name) {
-            SettingScreen(navController, isDark)
+        composable(route = SettingScreen.name) {
+            SettingScreen(navController, isDark, isDynamicColor)
         }
     }
 }
