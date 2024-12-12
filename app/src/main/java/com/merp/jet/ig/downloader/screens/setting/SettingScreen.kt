9d840d0234500db36af3ae49dc5c8f0afcab6016
PremDanej <danejprem005@gmail.com>
2024-12-12 17:25:28 +0530

    DynamicColor changes functionality added.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
@@ -1,5 +1,6 @@
 package com.merp.jet.ig.downloader.screens.setting
 
+import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,26 +11,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.merp.jet.ig.downloader.R.string.lbl_dark_mode
-import com.merp.jet.ig.downloader.R.string.lbl_light_mode
 import com.merp.jet.ig.downloader.R.string.lbl_setting
 import com.merp.jet.ig.downloader.components.BACKGROUND_COLOR
 import com.merp.jet.ig.downloader.components.ON_BACKGROUND_COLOR
@@ -38,7 +34,8 @@ import com.merp.jet.ig.downloader.components.ScreenDefault
 @Composable
 fun SettingScreen(
     navController: NavController,
-    isDark: MutableState<Boolean>
+    isDark: MutableState<Boolean>,
+    isDynamicColor: MutableState<Boolean>
 ) {
     ScreenDefault(
         title = stringResource(lbl_setting),
@@ -50,37 +47,60 @@ fun SettingScreen(
         Column(
             Modifier
                 .fillMaxSize()
-                .background(color = MaterialTheme.colorScheme.background),
+                .background(BACKGROUND_COLOR),
         ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                ChangeDynamicTheme(checked = isDynamicColor) {
+                    isDynamicColor.value = it
+                }
+            }
             ChangeTheme(checked = isDark) {
                 isDark.value = it
             }
         }
-
     }
 }
 
 @Composable
-fun ThemeOption(modifier: Modifier = Modifier) {
-    val themeOption = listOf(
-        "System default",
-        stringResource(lbl_light_mode),
-        stringResource(lbl_dark_mode)
-    )
-    val (selectOption, onOptionSelected) = remember {
-        mutableStateOf(themeOption[0])
-    }
-
-    Column {
-        themeOption.forEach { value ->
-            Row(modifier.fillMaxWidth(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
-                Text(text = value, color = MaterialTheme.colorScheme.onBackground)
-                RadioButton(
-                    selected = value == selectOption, onClick = { onOptionSelected(value) },
-                    colors = RadioButtonDefaults.colors(MaterialTheme.colorScheme.onBackground)
-                )
-            }
-        }
+fun ChangeDynamicTheme(checked: MutableState<Boolean>, onClick: (Boolean) -> Unit = {}) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        Arrangement.SpaceBetween,
+        Alignment.CenterVertically
+    ) {
+        Text(text = if (checked.value) "Turn off Dynamic theme" else "Turn on Dynamic theme")
+        Switch(
+            checked = checked.value,
+            onCheckedChange = {
+                onClick(it)
+            },
+            thumbContent = {
+                if (checked.value) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "Check",
+                        modifier = Modifier.size(SwitchDefaults.IconSize),
+                    )
+                } else {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Uncheck",
+                        modifier = Modifier.size(SwitchDefaults.IconSize),
+                    )
+                }
+            },
+            colors = SwitchDefaults.colors(
+                checkedBorderColor = ON_BACKGROUND_COLOR,
+                uncheckedBorderColor = ON_BACKGROUND_COLOR,
+                checkedIconColor = ON_BACKGROUND_COLOR,
+                checkedTrackColor = ON_BACKGROUND_COLOR,
+                uncheckedTrackColor = BACKGROUND_COLOR,
+                checkedThumbColor = BACKGROUND_COLOR,
+                uncheckedThumbColor = ON_BACKGROUND_COLOR,
+            )
+        )
     }
 }
 
@@ -93,7 +113,7 @@ fun ChangeTheme(checked: MutableState<Boolean>, onClick: (Boolean) -> Unit = {})
         Arrangement.SpaceBetween,
         Alignment.CenterVertically
     ) {
-        Text(text = if(checked.value) "Turn off dark mode" else "Turn on dark mode")
+        Text(text = if (checked.value) "Turn off dark mode" else "Turn on dark mode")
         Switch(
             checked = checked.value,
             onCheckedChange = {
@@ -103,13 +123,13 @@ fun ChangeTheme(checked: MutableState<Boolean>, onClick: (Boolean) -> Unit = {})
                 if (checked.value) {
                     Icon(
                         imageVector = Icons.Filled.DarkMode,
-                        contentDescription = null,
+                        contentDescription = "DarkMode",
                         modifier = Modifier.size(SwitchDefaults.IconSize),
                     )
-                }else{
+                } else {
                     Icon(
                         imageVector = Icons.Filled.LightMode,
-                        contentDescription = null,
+                        contentDescription = "LightMode",
                         modifier = Modifier.size(SwitchDefaults.IconSize),
                     )
                 }
