5de089912cf1a52e5a3715ee19d8312be934041c
PremDanej <danejprem005@gmail.com>
2024-12-23 19:42:37 +0530

    New constant added.

--- app/src/main/java/com/merp/jet/ig/downloader/utils/Constants.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.merp.jet.ig.downloader.utils
 
 object Constants {
     const val BASE_URL: String = "https://www.getindevice.com/wp-json/aio-dl/"
+    const val INSTA_REEL: String = "https://www.instagram.com/reel/"
 }
