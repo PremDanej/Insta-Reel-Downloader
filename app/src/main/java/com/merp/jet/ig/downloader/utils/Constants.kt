d7c24f1265c0bbbf1ad467821a6bd1a25c6c720b
PremDanej <danejprem005@gmail.com>
2024-12-26 20:30:02 +0530

    New constant added.
    Signed-off-by: PremDanej <danejprem005@gmail.com>

--- app/src/main/java/com/merp/jet/ig/downloader/utils/Constants.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/utils/Constants.kt
@@ -3,4 +3,8 @@ package com.merp.jet.ig.downloader.utils
 object Constants {
     const val BASE_URL: String = "https://www.getindevice.com/wp-json/aio-dl/"
     const val INSTA_REEL: String = "https://www.instagram.com/reel/"
+    const val INSTA_DATABASE: String = "insta_database"
+    const val INSTA_PREFERENCE: String = "insta_datastore"
+    const val IS_DARK: String = "is_dark"
+    const val IS_DYNAMIC_COLOR: String = "is_dynamic_color"
 }
