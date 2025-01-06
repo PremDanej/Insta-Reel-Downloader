4d1da399e43265592079d8e27016086a48cbf924
PremDanej <danejprem005@gmail.com>
2025-01-06 23:03:13 +0530

    Delay added to getSaveReels() for better response.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/save/SaveViewModel.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/save/SaveViewModel.kt
@@ -1,12 +1,15 @@
 package com.merp.jet.ig.downloader.screens.save
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.merp.jet.ig.downloader.model.ReelResponse
 import com.merp.jet.ig.downloader.repository.SaveRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
@@ -17,19 +20,18 @@ import javax.inject.Inject
 @HiltViewModel
 class SaveViewModel @Inject constructor(private val repository: SaveRepository) : ViewModel() {
 
+    var isLoading by mutableStateOf(true)
     private val _savedList = MutableStateFlow<List<ReelResponse>>(emptyList())
     val savedList = _savedList.asStateFlow()
 
-    init {
-        getSaveReels()
-    }
-
-    private fun getSaveReels() {
-        viewModelScope.launch(Dispatchers.IO) {
+    fun getSaveReels() {
+        viewModelScope.launch {
+            delay(250L)
             repository.getSaveReels().distinctUntilChanged()
                 .catch { e ->
                     Log.e("REEL", "SAVE_VIEWMODEL ERR | getSaveReels(): ${e.message}")
                 }.collect { list ->
+                    isLoading = false
                     _savedList.value = list.reversed()
                 }
         }
