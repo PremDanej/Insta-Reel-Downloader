9ee1f9f109c4d27b68856dd6b2cf49d24ccae685
PremDanej <danejprem005@gmail.com>
2025-01-22 17:31:10 +0530

    Email intent-filter and Support feature were added.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
@@ -1,5 +1,7 @@
 package com.merp.jet.ig.downloader.screens.setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.annotation.StringRes
 import androidx.compose.animation.core.Animatable
@@ -42,18 +44,21 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.merp.jet.ig.downloader.R.string.app_name
-import com.merp.jet.ig.downloader.R.string.lbl_about
+import com.merp.jet.ig.downloader.R.string.lbl_app_info
 import com.merp.jet.ig.downloader.R.string.lbl_change_download_location
 import com.merp.jet.ig.downloader.R.string.lbl_change_language
 import com.merp.jet.ig.downloader.R.string.lbl_clear_data
 import com.merp.jet.ig.downloader.R.string.lbl_dark_mode
+import com.merp.jet.ig.downloader.R.string.lbl_default_language
 import com.merp.jet.ig.downloader.R.string.lbl_dynamic_theme
 import com.merp.jet.ig.downloader.R.string.lbl_faq
 import com.merp.jet.ig.downloader.R.string.lbl_help
@@ -82,35 +87,59 @@ fun SettingScreen(
         navController = navController,
         onBackPressed = { navController.popBackStack() }
     ) {
-        val columnAnimation = remember { Animatable(0.9f) }
-        LaunchedEffect(true) {
-            columnAnimation.animateTo(1f, tween(300))
+
+        ScreenContent(navController, isDark, isDynamicColor)
+    }
+}
+
+@Composable
+fun ScreenContent(
+    navController: NavController,
+    isDark: MutableState<Boolean>,
+    isDynamicColor: MutableState<Boolean>
+) {
+    val columnAnimation = remember { Animatable(0.9f) }
+    LaunchedEffect(true) {
+        columnAnimation.animateTo(1f, tween(300))
+    }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .scale(columnAnimation.value)
+    ) {
+        HorizontalSpace(20.dp)
+        CategoryLabelText(lbl_setting_theme)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            ChangeDynamicTheme(checked = isDynamicColor.value) {
+                isDynamicColor.value = !isDynamicColor.value
+            }
         }
-        Column(
-            modifier = Modifier.fillMaxSize()
-                .verticalScroll(rememberScrollState())
-                .scale(columnAnimation.value)
-        ) {
-            HorizontalSpace(20.dp)
-            CategoryLabelText(lbl_setting_theme)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                ChangeDynamicTheme(checked = isDynamicColor.value) {
-                    isDynamicColor.value = !isDynamicColor.value
-                }
+        ChangeTheme(checked = isDark.value) { isDark.value = !isDark.value }
+        CategoryLabelWithDivider(lbl_setting_preference)
+        SubCategory(Icons.Outlined.DeleteForever, lbl_clear_data)
+        SubCategory(Icons.Outlined.RestartAlt, lbl_reset_setting)
+        SubCategory(
+            Icons.Outlined.Translate,
+            lbl_change_language,
+            context.getString(lbl_default_language)
+        )
+        SubCategory(Icons.Outlined.Folder, lbl_change_download_location, "InstaReels")
+        CategoryLabelWithDivider(lbl_setting_support)
+        SubCategory(Icons.Outlined.SupportAgent, lbl_setting_support) {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                setData(Uri.parse("mailto:"))
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("prem.dev@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Regarding IG Downloader app")
             }
-            ChangeTheme(checked = isDark.value) { isDark.value = !isDark.value }
-            CategoryLabelWithDivider(lbl_setting_preference)
-            SubCategory(Icons.Outlined.DeleteForever, lbl_clear_data)
-            SubCategory(Icons.Outlined.RestartAlt, lbl_reset_setting)
-            SubCategory(Icons.Outlined.Translate, lbl_change_language)
-            SubCategory(Icons.Outlined.Folder, lbl_change_download_location)
-            CategoryLabelWithDivider(lbl_setting_support)
-            SubCategory(Icons.Outlined.SupportAgent, lbl_setting_support)
-            SubCategory(Icons.AutoMirrored.Outlined.HelpOutline, lbl_help)
-            SubCategory(Icons.AutoMirrored.Outlined.LiveHelp, lbl_faq)
-            SubCategory(Icons.Outlined.Info, lbl_about){
-                navController.navigate(AboutScreen.name)
-            }
+            context.startActivity(intent)
+        }
+        SubCategory(Icons.AutoMirrored.Outlined.HelpOutline, lbl_help)
+        SubCategory(Icons.AutoMirrored.Outlined.LiveHelp, lbl_faq)
+        SubCategory(Icons.Outlined.Info, lbl_app_info) {
+            navController.navigate(AboutScreen.name)
         }
     }
 }
@@ -118,7 +147,10 @@ fun SettingScreen(
 @Composable
 private fun CategoryLabelText(@StringRes resId: Int = app_name) {
     Text(
-        modifier = Modifier.fillMaxWidth().padding(start = 10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp)
+            .alpha(0.6f),
         text = stringResource(id = resId),
         style = MaterialTheme.typography.bodySmall // try - titleMedium, bodyLarge
             .copy(fontWeight = FontWeight.SemiBold)
@@ -148,7 +180,9 @@ private fun ChangeDynamicTheme(checked: Boolean, onClick: () -> Unit = {}) {
         onClick()
     }) {
         Row(
-            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             IconButton(
@@ -163,7 +197,9 @@ private fun ChangeDynamicTheme(checked: Boolean, onClick: () -> Unit = {}) {
             }
             Text(
                 text = stringResource(lbl_dynamic_theme),
-                modifier = Modifier.weight(1f).padding(start = 10.dp)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp)
             )
             Switch(
                 checked = checked,
@@ -197,7 +233,9 @@ private fun ChangeTheme(checked: Boolean, onClick: () -> Unit = {}) {
         onClick()
     }) {
         Row(
-            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             IconButton(
@@ -212,7 +250,9 @@ private fun ChangeTheme(checked: Boolean, onClick: () -> Unit = {}) {
             }
             Text(
                 text = stringResource(lbl_dark_mode),
-                modifier = Modifier.weight(1f).padding(start = 10.dp)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp)
             )
             Switch(
                 checked = checked,
@@ -244,13 +284,16 @@ private fun ChangeTheme(checked: Boolean, onClick: () -> Unit = {}) {
 private fun SubCategory(
     icon: ImageVector? = null,
     @StringRes resId: Int = app_name,
+    extraValue: String? = null,
     onClick: () -> Unit = {}
 ) {
     Box(modifier = Modifier.clickable {
         onClick()
     }) {
         Row(
-            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             IconButton(
@@ -267,8 +310,20 @@ private fun SubCategory(
             }
             Text(
                 text = stringResource(id = resId),
-                modifier = Modifier.weight(1f).padding(start = 10.dp)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp)
             )
+            extraValue?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.bodySmall
+                        .copy(fontWeight = FontWeight.SemiBold),
+                    modifier = Modifier
+                        .padding(end = 10.dp)
+                        .alpha(0.6f)
+                )
+            }
         }
     }
 }
