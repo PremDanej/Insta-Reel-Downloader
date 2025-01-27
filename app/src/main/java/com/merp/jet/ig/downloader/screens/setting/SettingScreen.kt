0de104786e5e8f41c3b0e374f0f29307d11ecc7a
PremDanej <danejprem005@gmail.com>
2025-01-27 15:59:50 +0530

    Managed dialog for language choice.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/setting/SettingScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -37,17 +38,25 @@ import androidx.compose.material.icons.outlined.Translate
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -57,6 +66,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -88,6 +98,9 @@ import com.merp.jet.ig.downloader.components.LoadingButton
 import com.merp.jet.ig.downloader.components.ON_BACKGROUND_COLOR
 import com.merp.jet.ig.downloader.components.ScreenDefault
 import com.merp.jet.ig.downloader.navigation.InstaReelScreens.AboutScreen
+import com.merp.jet.ig.downloader.utils.Constants.DEFAULT_EN_LOCALE_CODE
+import com.merp.jet.ig.downloader.utils.Constants.HINDI_LOCALE_CODE
+import com.merp.jet.ig.downloader.utils.Utils.setAppLanguage
 import com.merp.jet.ig.downloader.utils.Utils.showToast
 
 @Composable
@@ -108,6 +121,7 @@ fun SettingScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(
     viewModel: SettingViewModel,
@@ -120,6 +134,8 @@ fun ScreenContent(
         columnAnimation.animateTo(1f, tween(300))
     }
     val context = LocalContext.current
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     val dialogOpen = remember {
         mutableStateOf(false)
@@ -134,6 +150,17 @@ fun ScreenContent(
                 value = false
             },
         )
+    }
+
+    if (showBottomSheet) {
+        AppLanguageBottomSheet(
+            sheetState = sheetState,
+            selectedLanguage = viewModel.getLanguage() ?: DEFAULT_EN_LOCALE_CODE,
+            onDismissRequest = { showBottomSheet = false },
+        ){ languageCode ->
+            viewModel.setLanguage(languageCode)
+            setAppLanguage(context,languageCode)
+        }
     }
 
     Column(
@@ -165,9 +192,18 @@ fun ScreenContent(
             dialogOpen.value = !dialogOpen.value
         }
 
-        SubCategory(Icons.Outlined.RestartAlt, lbl_reset_setting)
+        SubCategory(Icons.Outlined.RestartAlt, lbl_reset_setting) {
+            viewModel.setLanguage(DEFAULT_EN_LOCALE_CODE)
+            setAppLanguage(context, DEFAULT_EN_LOCALE_CODE)
+        }
 
-        SubCategory(Icons.Outlined.Translate, lbl_change_language, context.getString(lbl_default_language))
+        SubCategory(
+            icon = Icons.Outlined.Translate,
+            resId = lbl_change_language,
+            extraValue = context.getString(lbl_default_language)
+        ) {
+            showBottomSheet = true
+        }
 
         SubCategory(Icons.Outlined.Folder, lbl_change_download_location, "InstaReels")
 
@@ -394,12 +430,7 @@ fun DeleteDialog(
         modifier = Modifier
             .fillMaxWidth(0.85f)
             .scale(dialogAnimation.value)
-            .let { modifier ->
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
-                    modifier
-                        .border(0.1.dp, ON_BACKGROUND_COLOR, RoundedCornerShape(12))
-                } else modifier
-            },
+            .border(0.1.dp, ON_BACKGROUND_COLOR, RoundedCornerShape(12)),
         shape = RoundedCornerShape(12),
         properties = DialogProperties(
             usePlatformDefaultWidth = false,
@@ -445,4 +476,83 @@ fun DeleteDialog(
             }
         }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppLanguageBottomSheet(
+    sheetState: SheetState,
+    selectedLanguage: String? = null,
+    onDismissRequest: () -> Unit = {},
+    onLanguageChange: (String) -> Unit = {}
+) {
+    ModalBottomSheet(
+        modifier = Modifier
+            .padding(top = 50.dp),
+        scrimColor = ON_BACKGROUND_COLOR.copy(0.3f),
+        sheetState = sheetState,
+        containerColor = BACKGROUND_COLOR,
+        onDismissRequest = { onDismissRequest() }
+    ) {
+
+        val radioLanguages = listOf(
+            listOf("English", DEFAULT_EN_LOCALE_CODE),
+            listOf("Hindi", HINDI_LOCALE_CODE),
+        )
+        val (selectedOption, onOptionSelected) = remember {
+            mutableStateOf(selectedLanguage)
+        }
+
+        Text(
+            text = "App Language",
+            Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleMedium
+        )
+        HorizontalSpace()
+        Divider()
+        HorizontalSpace()
+
+        radioLanguages.forEach { language ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (language.last() == selectedOption),
+                        onClick = {
+                            onOptionSelected(language.last())
+                            onLanguageChange(language.last())
+                            onDismissRequest()
+                        }
+                    ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    modifier = Modifier.padding(10.dp, 5.dp),
+                    selected = language.last() == selectedOption,
+                    onClick = {
+                        onOptionSelected(language.last())
+                        onLanguageChange(language.last())
+                        onDismissRequest()
+                    },
+                    colors = RadioButtonDefaults.colors(
+                        selectedColor = ON_BACKGROUND_COLOR,
+                        unselectedColor = ON_BACKGROUND_COLOR.copy(0.5f)
+                    )
+                )
+
+                Column {
+                    Text(text = language[0])
+                    Text(
+                        modifier = Modifier.alpha(0.7f),
+                        text = language[1],
+                        style = MaterialTheme.typography.bodySmall
+                            .copy(fontWeight = FontWeight.SemiBold)
+                    )
+                }
+            }
+            HorizontalSpace(10.dp)
+        }
+        HorizontalSpace()
+    }
 }
