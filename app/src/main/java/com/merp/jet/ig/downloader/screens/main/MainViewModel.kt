f7fddf59eff0a0a1a5989b7d2db15690141ef9cd
PremDanej <danejprem005@gmail.com>
2025-05-05 17:51:05 +0530

    Add color-language code and remove static imports.

--- app/src/main/java/com/merp/jet/ig/downloader/screens/main/MainViewModel.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/screens/main/MainViewModel.kt
@@ -3,7 +3,10 @@ package com.merp.jet.ig.downloader.screens.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.merp.jet.ig.downloader.data.preference.InstaReelDataStore
-import com.merp.jet.ig.downloader.utils.Constants
+import com.merp.jet.ig.downloader.utils.Constants.DEFAULT_EN_LOCALE_CODE
+import com.merp.jet.ig.downloader.utils.Constants.IS_DARK
+import com.merp.jet.ig.downloader.utils.Constants.IS_DYNAMIC_COLOR
+import com.merp.jet.ig.downloader.utils.Constants.LOCALE_CODE
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,21 +19,31 @@ class MainViewModel @Inject constructor(private val instaReelDataStore: InstaRee
 
     fun setDark(value: Boolean = false) {
         viewModelScope.launch(Dispatchers.IO) {
-            instaReelDataStore.putBoolean(Constants.IS_DARK, value)
+            instaReelDataStore.putBoolean(IS_DARK, value)
         }
     }
 
     fun getDark(): Boolean? = runBlocking {
-        instaReelDataStore.getBoolean(Constants.IS_DARK)
+        instaReelDataStore.getBoolean(IS_DARK)
     }
 
     fun setDynamicColor(value: Boolean = false) {
         viewModelScope.launch(Dispatchers.IO) {
-            instaReelDataStore.putBoolean(Constants.IS_DYNAMIC_COLOR, value)
+            instaReelDataStore.putBoolean(IS_DYNAMIC_COLOR, value)
         }
     }
 
     fun getDynamicColor(): Boolean? = runBlocking {
-        instaReelDataStore.getBoolean(Constants.IS_DYNAMIC_COLOR)
+        instaReelDataStore.getBoolean(IS_DYNAMIC_COLOR)
+    }
+
+    fun setLanguage(value: String = DEFAULT_EN_LOCALE_CODE) {
+        viewModelScope.launch {
+            instaReelDataStore.putString(LOCALE_CODE, value)
+        }
+    }
+
+    fun getLanguage(): String? = runBlocking {
+        instaReelDataStore.getString(LOCALE_CODE)
     }
 }
