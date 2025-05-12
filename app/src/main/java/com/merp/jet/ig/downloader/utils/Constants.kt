4729772d368458ae901864cda9252b555e472316
PremDanej <danejprem005@gmail.com>
2025-05-13 02:27:28 +0530

    update constant.

--- app/src/main/java/com/merp/jet/ig/downloader/utils/Constants.kt
+++ app/src/main/java/com/merp/jet/ig/downloader/utils/Constants.kt
@@ -1,7 +1,9 @@
 package com.merp.jet.ig.downloader.utils
 
+import com.merp.jet.ig.downloader.BuildConfig
+
 object Constants {
-    const val BASE_URL: String = "https://www.getindevice.com/wp-json/aio-dl/"
+    const val BASE_URL: String = BuildConfig.BASE_URL
     const val INSTA_REEL: String = "https://www.instagram.com/reel/"
     const val INSTA_DATABASE: String = "insta_database"
     const val INSTA_PREFERENCE: String = "insta_datastore"
@@ -9,4 +11,5 @@ object Constants {
     const val IS_DYNAMIC_COLOR: String = "is_dynamic_color"
     const val DEFAULT_EN_LOCALE_CODE: String = "en"
     const val LOCALE_CODE: String = "locale_code"
+    const val HINDI_LOCALE_CODE: String = "hi"
 }
